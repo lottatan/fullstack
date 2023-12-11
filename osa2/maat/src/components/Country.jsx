81311e31cf99a7b6b771c5d849873f0b934b0969
Lotta Tanner <lotta.tanner@helsinki.fi>
2023-12-11 13:07:21 +0200

    osa2

--- osa2/maat/src/components/Country.jsx
+++ osa2/maat/src/components/Country.jsx
@@ -1,6 +1,34 @@
+import React, { useState, useEffect } from 'react'
+
 const Country = ( {country} ) => {
     const languages = Object.values(country.languages)
     const flag = country.flags.png
+    const api_key = import.meta.env.VITE_SOME_KEY
+    const [weather, setWeather] = useState(null)
+    const [weatherIcon, setWeatherIcon] = useState('')
+
+    useEffect(() => {
+        const fetchWeather = async () => {
+          try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital[0]}&appid=${api_key}&units=metric`)
+            if (response.ok) {
+              const data = await response.json()
+              setWeather(data)
+
+              if (data.weather && data.weather.length > 0) {
+                const iconCode = data.weather[0].icon
+                setWeatherIcon(`https://openweathermap.org/img/wn/${iconCode}.png`)}            
+            }}
+              
+          catch (error) {
+            console.error('Error fetching weather data:', error)}
+        }
+      
+        if (country.capital[0] && api_key) {
+          fetchWeather()
+        }
+      }, [country.capital[0], api_key])      
+        
 
     return(
         <div>
@@ -16,6 +44,13 @@ const Country = ( {country} ) => {
         )}
         </ul>
         <img src={flag} width="100px" height="auto"></img>
+        <h2>Weather in {country.capital[0]}</h2>
+        {weather && weather.main && (
+            <div>
+            <p>Temperature: {weather.main.temp} °C</p>
+            {weatherIcon && (<img src={weatherIcon} alt="Weather Icon" />)}
+            <p>Wind: {weather.wind.speed} m/s</p>
+            </div>)}
         </div>
     )
 }
