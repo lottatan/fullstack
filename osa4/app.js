d797a4db0ce0ea8e79038c15cce67eb595605222
Lotta Tanner <lotta.tanner@helsinki.fi>
2024-01-16 14:37:11 +0200

    5.22

--- osa4/app.js
+++ osa4/app.js
@@ -6,6 +6,11 @@ const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 require('express-async-errors')
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
@@ -29,10 +34,10 @@ app.use(express.static('build'))
 app.use(middleware.requestLogger)
 
 app.use(middleware.tokenExtractor)
-app.use(middleware.userExtractor)
-app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.userExtractor)
 
 
 app.use(middleware.unknownEndpoint)
